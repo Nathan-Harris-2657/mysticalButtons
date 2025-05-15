242f236d2b54687d912ca5a21acb4731bdff025a
Nathan Harris <singdubharris@gmail.com>
2025-05-15 09:25:55 -0400

    level1progress

--- NewButtonGame/Level1/level1.js
+++ NewButtonGame/Level1/level1.js
@@ -1,8 +1,41 @@
-const body = document.querySelector('body')
-const fire = document.getElementById('fire')
+const messageContainer = document.getElementById('messageContainer')
+const gameContainer = document.getElementById('gameContainer')
 
-body.id.add('fireMode')
+document.addEventListener('DOMContentLoaded',() =>{
+    const message = document.createElement('div')
+    message.setAttribute('id', 'prolog')
+    message.innerHTML ="In the silent corridors of fate, where shadows whisper and time holds its breath,<br/> the right decision is the key that unlocks unseen doors revealing paths only the wise can walk."
+    message.style.opacity = '0'
+    message.style.transition = 'all 1s ease-in-out'
+    messageContainer.appendChild(message)
+    setInterval(() => {
+        message.style.opacity = '1'
+            setInterval(() => {
+                message.style.opacity = '0'
+            }, 6000);
+    }, 2000);
+})
 
-fire.addEventListener('click', () =>{
-    body.style.toggle('#fireMode')
+document.addEventListener('DOMContentLoaded', ()=>{
+    const inputContainer = document.createElement('div')
+    inputContainer.setAttribute('id', 'inputContainer')
+    inputContainer.style.opacity = '0'
+    inputContainer.style.transition = 'all 1s ease'
+
+    const getName = document.createElement('input')
+    getName.type = 'text'
+    getName.setAttribute('id', 'getName')
+
+    const type = document.createElement('div')
+    type.setAttribute('id', 'type')
+    type.textContent = "What is Your Name:"
+
+        gameContainer.append(inputContainer)
+        inputContainer.appendChild(type)
+        inputContainer.appendChild(getName)
+    setInterval(() => {
+        inputContainer.style.opacity = '1'
+    }, 4000);
+    
+    
 })
