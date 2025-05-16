4c5be02e8510a24e2cecbbede568f79364d9df0f
Nathan Harris <singdubharris@gmail.com>
2025-05-15 21:18:07 -0400

    working

--- NewButtonGame/Level1/level1.js
+++ NewButtonGame/Level1/level1.js
@@ -1,20 +1,33 @@
 const messageContainer = document.getElementById('messageContainer')
 const gameContainer = document.getElementById('gameContainer')
 
+const inputContainer = document.getElementById('inputContainer')
+
 const pleaseEnterName = document.getElementById('pleaseEnterName')
 const inputName = document.getElementById('inputName')
 
-const player = {}
+const player = []
 
 inputName.addEventListener('keydown',(event)=>{
     if(event.key === 'Enter'){
         const playerName = inputName.value
-        player.name = playerName
-    gameContainer.remove(messageContainer)
-    
-    }
+        player.push(playerName)
+    inputContainer.removeChild(pleaseEnterName)
+    inputContainer.removeChild(inputName)
+    const message2 = document.createElement('div')
+    message2.setAttribute('id','prolog2')
+    message2.innerHTML = `"Nice to meet you" ${player}` 
+    message2.style.opacity = '0'
+    message2.style.transition = 'all 1s ease-in-out'
+    messageContainer.appendChild(message2)
+    setTimeout(() => {
+    message2.style.opacity = '1';
+    setTimeout(() => {
+        message2.style.opacity = '0';
+    }, 6000);
+}, 2000);
 
-    
+    }
 })
 
 
@@ -26,14 +39,18 @@ document.addEventListener('DOMContentLoaded',() =>{
     message.style.opacity = '0'
     message.style.transition = 'all 1s ease-in-out'
     messageContainer.appendChild(message)
-    setInterval(() => {
-        message.style.opacity = '1'
-            setInterval(() => {
-                message.style.opacity = '0'
-            }, 6000);
-    }, 2000);
+    setTimeout(() => {
+    message.style.opacity = '1';
+    setTimeout(() => {
+        message.style.opacity = '0';
+        setTimeout(() => {
+            message.remove()
+        }, 6500);
+    }, 6000);
+}, 2000);
+
 })
-console.log(player.name)
+
 
 
 
